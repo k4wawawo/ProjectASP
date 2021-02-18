c3ebd2f19523292687b7d241c7f54848cf47b9fc
k4wawawo <waldemar.wozniak@microsoft.wsei.edu.pl>
2021-02-18 20:37:08 +0100

    add pages functionality

---
+++ Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,36 @@
+﻿using Project_ASP.Models.Data;
+using Project_ASP.Models.ViewModel.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_ASP.Areas.Admin.Controllers
+{ 
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            // declare list of pagesvm
+            List<PageVM> pagesList;
+            
+            using (Db db = new Db())
+            {
+                // init the list
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            // return view with list
+            return View(pagesList);
+        }
+
+        // GET: Admin/Pages/AddPage
+        public ActionResult AddPage()
+
+        {
+            return View();
+        }
+    }
+} 
