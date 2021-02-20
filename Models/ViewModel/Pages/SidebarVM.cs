0a01e59960c0ebbbc181248c8e0ec9faf6b196a9
k4wawawo <waldemar.wozniak@microsoft.wsei.edu.pl>
2021-02-20 19:26:29 +0100

    Add Fileman CKEditor -A.Pages fini

--- Models/ViewModel/Pages/SidebarVM.cs
+++ Models/ViewModel/Pages/SidebarVM.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Project_ASP.Models.ViewModel.Pages
 {
@@ -21,6 +22,7 @@ namespace Project_ASP.Models.ViewModel.Pages
         }
 
         public int Id { get; set; }
+        [AllowHtml]
         public string Body { get; set; }
     }
 }
