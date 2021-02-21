fc4c27f12d5405c7e97b040e8dd4c95914ac17be
k4wawawo <waldemar.wozniak@microsoft.wsei.edu.pl>
2021-02-21 23:30:35 +0100

    Add Categorypartial

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,6 +13,9 @@ namespace Project_ASP
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "Shop", action = "Index", name = UrlParameter.Optional }, new[] { "Project_ASP.Controllers" });
+
+            routes.MapRoute("SidebarPartial", "Pages/SidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "Project_ASP.Controllers" });
             routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] { "Project_ASP.Controllers" });
             routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "Project_ASP.Controllers" });
             routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "Project_ASP.Controllers" });
---
+++ Controllers/ShopController.cs
@@ -0,0 +1,34 @@
+﻿using Project_ASP.Models.Data;
+using Project_ASP.Models.ViewModel.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_ASP.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Pages");
+        }
+
+        public ActionResult CategoryMenuPartial()
+        {
+            // Declare list of CategoryVM
+            List<CategoryVM> categoryVMList;
+
+            // Init the list
+            using (Db db = new Db())
+            {
+                categoryVMList = db.Categories.ToArray().OrderBy(x => x.Sorting).Select(x => new CategoryVM(x)).ToList();
+            }
+
+            // Return partial with list
+            return PartialView(categoryVMList);
+        }
+    }
+}
