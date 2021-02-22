1f0769937f0d64bfca3136b01d57b71dad782de8
k4wawawo <waldemar.wozniak@microsoft.wsei.edu.pl>
2021-02-22 23:36:03 +0100

    Add pst.bck method create account

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,11 @@
-﻿using Project_ASP.Models.ViewModel.Account;
+﻿using Project_ASP.Models.Data;
+using Project_ASP.Models.ViewModel.Account;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Project_ASP.Controllers
 {
@@ -30,6 +32,40 @@ namespace Project_ASP.Controllers
             return View();
         }
 
+        // POST: /account/login
+        [HttpPost]
+        public ActionResult Login(LoginUserVM model)
+        {
+            // Check model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // Check if the user is valid
+
+            bool isValid = false;
+
+            using (Db db = new Db())
+            {
+                if (db.Users.Any(x => x.Username.Equals(model.Username) && x.Password.Equals(model.Password)))
+                {
+                    isValid = true;
+                }
+            }
+
+            if (!isValid)
+            {
+                ModelState.AddModelError("", "Invalid username or password.");
+                return View(model);
+            }
+            else
+            {
+                FormsAuthentication.SetAuthCookie(model.Username, model.RememberMe);
+                return Redirect(FormsAuthentication.GetRedirectUrl(model.Username, model.RememberMe));
+            }
+        }
+
         // GET: /account/create-account
         [ActionName("create-account")]
         [HttpGet]
@@ -38,5 +74,69 @@ namespace Project_ASP.Controllers
             return View("CreateAccount");
         }
 
+        // POST: /account/create-account
+        [ActionName("create-account")]
+        [HttpPost]
+        public ActionResult CreateAccount(UserVM model)
+        {
+            // Check model state
+            if (!ModelState.IsValid)
+            {
+                return View("CreateAccount", model);
+            }
+
+            // Check if passwords match
+            if (!model.Password.Equals(model.ConfirmPassword))
+            {
+                ModelState.AddModelError("", "Passwords do not match.");
+                return View("CreateAccount", model);
+            }
+
+            using (Db db = new Db())
+            {
+                // Make sure username is unique
+                if (db.Users.Any(x => x.Username.Equals(model.Username)))
+                {
+                    ModelState.AddModelError("", "Username " + model.Username + " is taken.");
+                    model.Username = "";
+                    return View("CreateAccount", model);
+                }
+
+                // Create userDTO
+                UserDTO userDTO = new UserDTO()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    EmailAddress = model.EmailAddress,
+                    Username = model.Username,
+                    Password = model.Password
+                };
+
+                // Add the DTO
+                db.Users.Add(userDTO);
+
+                // Save
+                db.SaveChanges();
+
+                // Add to UserRolesDTO
+                int id = userDTO.Id;
+
+                UserRoleDTO userRolesDTO = new UserRoleDTO()
+                {
+                    UserId = id,
+                    RoleId = 2
+                };
+
+                db.UserRoles.Add(userRolesDTO);
+                db.SaveChanges();
+            }
+
+            // Create a TempData message
+            TempData["SM"] = "You are now registered and can login.";
+
+            // Redirect
+            return Redirect("~/account/login");
+        }
     }
 }
+
