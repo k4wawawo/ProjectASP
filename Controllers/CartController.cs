46659f5185b1f07524ce6439e1b4751bcf75bc22
k4wawawo <waldemar.wozniak@microsoft.wsei.edu.pl>
2021-02-22 20:32:49 +0100

    Add cart controller.partial

---
+++ Controllers/CartController.cs
@@ -0,0 +1,54 @@
+﻿using Project_ASP.Models.ViewModel.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_ASP.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CartPartial()
+        {
+            // Init CartVM
+            CartVM model = new CartVM();
+
+            // Init quantity
+            int qty = 0;
+
+            // Init price
+            decimal price = 0m;
+
+            // Check for cart session
+            if (Session["cart"] != null)
+            {
+                // Get total qty and price
+                var list = (List<CartVM>)Session["cart"];
+
+                foreach (var item in list)
+                {
+                    qty += item.Quantity;
+                    price += item.Quantity * item.Price;
+                }
+
+                model.Quantity = qty;
+                model.Price = price;
+            }
+            else
+            {
+                // Or set qty and price to 0
+                model.Quantity = 0;
+                model.Price = 0m;
+            }
+            // Return partial view with model
+            return PartialView(model);
+        }
+    }
+}
