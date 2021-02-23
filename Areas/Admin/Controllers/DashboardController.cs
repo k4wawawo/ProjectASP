d16c39f7a857a59c5d1341a477ea2615cc3dd805
k4wawawo <waldemar.wozniak@microsoft.wsei.edu.pl>
2021-02-23 20:33:36 +0100

    add users views orders and final checks

--- Areas/Admin/Controllers/DashboardController.cs
+++ Areas/Admin/Controllers/DashboardController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace Project_ASP.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class DashboardController : Controller
     {
         // GET: Admin/Dashboard
--- Areas/Admin/Controllers/PagesController.cs
+++ Areas/Admin/Controllers/PagesController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 
 namespace Project_ASP.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class PagesController : Controller
     {
         // GET: Admin/Pages
